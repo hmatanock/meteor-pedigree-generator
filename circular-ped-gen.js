fb56ea3f53bbd5d48a06e6b41f9847607a1a82d5
Heather Matanock <hmatanock@bimedia.net>
2014-02-17 01:06:12 -0600

    Progress

--- circular-ped-gen.js
+++ circular-ped-gen.js
@@ -2,14 +2,28 @@
 Dogs = new Meteor.Collection("Dogs");
 Peds = new Meteor.Collection("Peds");
 
+var currentPedigree;
+
+var getCurrentUserEmail = function(){
+  return Meteor.user() &&
+    Meteor.user().emails &&
+    Meteor.user().emails[0].address;
+};
+
 
 if (Meteor.isClient) {
-
-  Template.pedigree.currentPedigree = function(){
-    return Peds.find({_id: Session.get('currentPedigree')});
+  Template.pedigree.editActive = function(){
+    return Session.get("currentPedigree");
   };
 
-  Template.pedigree.getDog = function(dogName) {
+  Template.pedigree.currentPedigree = function(){
+    currentPedigree = Peds.find({_id: Session.get('currentPedigree')}).fetch();
+    dog = Dogs.find({"dogName": currentPedigree[0].startingDog});
+    return "Working on Pedigree for " + dog[0].dogName;
+  };
+
+  Template.pedigree.allPeds = function() {
+    return Peds.find({'user':getCurrentUserEmail()}).fetch();
 
   };
 
@@ -19,9 +33,27 @@ if (Meteor.isClient) {
 
   Template.pedigree.events({
     "click #start" : function(evt, templ){
-        var startingDog = templ.find("#startingDog").value;
-        var curPed = Peds.insert({'startingDog':startingDog});
-        Session.set("currentPedigree", curPed._id);
+        var prefixes = templ.find("#prefixTitles").value;
+        var dogname = templ.find("#dogName").value;
+        var suffixes = templ.find("#suffixTitles").value;
+        console.log(prefixes);
+        console.log(dogname);
+        console.log(suffixes);
+        var dogId = Dogs.insert({
+          'prefixTitles':prefixes,
+          'dogName':dogname,
+          'suffixTitles':suffixes
+        });
+        var curPedId = Peds.insert({
+          'startingDog':dogname,
+          'user':getCurrentUserEmail(),
+        });
+        Session.set("currentPedigree", curPedId);
+    },
+    "click #new" : function(evt, templ){
+        Session.set("currentPedigree", null);
+        templ.find("#newPed").className="show";
+
     }
   });
 }
